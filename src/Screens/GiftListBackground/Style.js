ef30e5d3c0d5563452c9096b229224355acefe95
Amanda Hughes <amanda@trevi-app.com>
2025-07-27 13:53:32 +0100

    ✅ Resolve merge conflict in GiftListBackground styles

--- src/Screens/GiftListBackground/Style.js
+++ src/Screens/GiftListBackground/Style.js
@@ -3,21 +3,12 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   background: {
     flex: 1,
-<<<<<<< HEAD
     width: '100%',
     height: '100%',
   },
   overlay: {
     flex: 1,
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-  },
-});
-=======
-  },
-  overlay: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.6)',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', // You can change this to (0,0,0,0.6) if you want a darker overlay
     paddingTop: 60,
   },
 });
->>>>>>> cf009af (Add GiftList screen no Stripe connect yet)
