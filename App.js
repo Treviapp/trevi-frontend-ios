eb6cd46908814e19e1046af34ab8fc3fa7d85bbc
Amanda Hughes <amanda@trevi-app.com>
2025-09-18 17:04:56 +0100

    fix root import to Routes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
 import { registerRootComponent } from 'expo';
-import App from './src/navigation/Routes';
+import Routes from './src/navigation/Routes';
 
-registerRootComponent(App);
+registerRootComponent(Routes);
